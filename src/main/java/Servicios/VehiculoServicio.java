c21d18c0ae57718d1f6ed9ed241c99d8de7c97b9
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2023-04-12 12:36:33 -0300

    servicio

--- src/main/java/Servicios/VehiculoServicio.java
+++ src/main/java/Servicios/VehiculoServicio.java
@@ -1,4 +1,26 @@
 package Servicios;
 
+
+//traer logica dao
 public class VehiculoServicio {
+
+    private final VehiculoDao vehiculoDao;
+
+    public VehiculoServicio(){ vehiculoDao = new VehiculoServicio();}
+
+
+    //metodo para imprimir con try-catch exception
+    public void ImprimirVehiculo() throws Exception {
+
+        try {
+            List(vehiculo.isEmpty()){
+                throw new Excepetion("El vehiculo no existe.")
+            }else{
+                System.out.println("Lista vehiculos: ");
+                System.out.println("");
+            }
+        }catch (Exception e){
+            throw e;
+        }
+    }
 }
