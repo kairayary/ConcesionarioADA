6a34c8535e54f2090e6264361b0e58579f888d96
Ara Chavez <arichavez0507@gmail.com>
2023-04-14 16:20:03 -0600

    Datos faltantes

--- src/main/java/Entidad/Vehiculo.java
+++ src/main/java/Entidad/Vehiculo.java
@@ -1,4 +1,60 @@
 package Entidad;
-
 public class Vehiculo {
+
+    private String marca;
+    private String modelo;
+    private String color;
+    private int stock_disponible;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String marca, String modelo, String color, int stock_disponible) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.stock_disponible = stock_disponible;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getStock_disponible() {
+        return stock_disponible;
+    }
+
+    public void setStock_disponible(int stock_disponible) {
+        this.stock_disponible = stock_disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", stock_disponible=" + stock_disponible +
+                '}';
+    }
 }
